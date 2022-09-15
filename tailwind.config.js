c0bb82bed86e599d8e67f6d08f9fdfc8f8f392de
Leiko Lei <chachafrdark@gmail.com>
2022-09-15 11:50:04 +0200

    added a tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ], 
   theme: {
+	  colors: {
+	  	"grey": "#676767",
+		"black": "#000000",
+		"red": "#f43841",
+		"green": "#73c936",
+		"yellow": "#ffdd33",
+		"blue": "#96a6c8",
+		"magenta": "#9e95c7",
+		"cyan": "#95a99f",
+		"white": "#ffffff",
+	  },
     extend: {},
   },
   plugins: [],
